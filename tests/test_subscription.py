b73b319cd4e6c3d200798ce5585bd52dcb176f17
Bohdan Honchar <bohdan26062003@gmail.com>
2024-09-08 23:01:19 +0300

    Test is updated

--- tests/test_subscription.py
+++ tests/test_subscription.py
@@ -12,22 +12,22 @@ class TestSubscription:
     def test_verify_subscription_in_home_page(self, home_page):
         with allure.step("Scroll down to footer"):
             home_page.scroll_down_to_footer()
-            take_screenshot(home_page.page, "Scroll down to footer")
+            take_screenshot(home_page.page,"Scroll down to footer")
 
         with allure.step("Verify text 'SUBSCRIPTION'"):
             expect(home_page.get_subscription_header()).to_be_visible()
-            expect(home_page.get_subscription_header()).to_have_text("SUBSCRIPTION")
+            take_screenshot(home_page.page,"Verify text 'SUBSCRIPTION'")
 
         faker = Faker()
         with allure.step("Enter email address in input and click arrow button"):
+            take_screenshot(home_page.page,"Enter email address in input and click arrow button")
             home_page.fill_subscription_input(faker.email())
             home_page.click_subscription_button()
-            take_screenshot(home_page.page, "Enter email address in input and click arrow button")
 
         with allure.step("Verify success message 'You have been successfully subscribed!' is visible"):
+            take_screenshot(home_page.page,"Verify success message 'You have been successfully subscribed!' is visible")
             expect(home_page.get_subscription_success_message()).to_be_visible()
             expect(home_page.get_subscription_success_message()).to_have_text("You have been successfully subscribed!")
-            take_screenshot(home_page.page, "Verify success message 'You have been successfully subscribed!' is visible")
 
     @allure.title("Verify Subscription in cart page")
     def test_verify_subscription_in_cart_page(self, home_page, cart_page):
@@ -41,8 +41,8 @@ class TestSubscription:
             take_screenshot(home_page.page, "Scroll down to footer")
 
         with allure.step("Verify text 'SUBSCRIPTION'"):
+            take_screenshot(home_page.page, "Verify text 'SUBSCRIPTION'")
             expect(home_page.get_subscription_header()).to_be_visible()
-            expect(home_page.get_subscription_header()).to_have_text("SUBSCRIPTION")
 
         faker = Faker()
         with allure.step("Enter email address in input and click arrow button"):
@@ -51,6 +51,6 @@ class TestSubscription:
             take_screenshot(home_page.page, "Enter email address in input and click arrow button")
 
         with allure.step("Verify success message 'You have been successfully subscribed!' is visible"):
+            take_screenshot(home_page.page, "Verify success message 'You have been successfully subscribed!' is visible")
             expect(home_page.get_subscription_success_message()).to_be_visible()
             expect(home_page.get_subscription_success_message()).to_have_text("You have been successfully subscribed!")
-            take_screenshot(home_page.page, "Verify success message 'You have been successfully subscribed!' is visible")
